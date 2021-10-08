825cbdc1b2bcaa18aa068a9225a90dc459481573
Elias Martinez <elias.martinez@isolutions.ch>
2021-10-08 21:13:42 +0200

    geo location

--- src/backend/Tortilla.Hackathon.Services/Services/GeolocationService.cs
+++ src/backend/Tortilla.Hackathon.Services/Services/GeolocationService.cs
@@ -1,5 +1,6 @@
-﻿using Geocoding;
-using Geocoding.Google;
+﻿using Geocoding.Microsoft;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
 using Tortilla.Hackathon.Services.Interfaces;
 
@@ -7,13 +8,25 @@ namespace Tortilla.Hackathon.Services.Services
 {
     public class GeolocationService : IGeolocationService
     {
+        private readonly BingMapsGeocoder geocoder;
+
+        public GeolocationService(IConfiguration configuration)
+        {
+            var apiKey = configuration["BingGeocodingApiKey"];
+            geocoder = new BingMapsGeocoder(apiKey);
+        }
+
         public async Task<string> GetLocationDescription(double latitude, double longitude)
         {
-            var geocoder = new GoogleGeocoder() { ApiKey = "this-is-my-google-api-key" };
+            var addresses = await geocoder.ReverseGeocodeAsync(latitude, longitude);
+            var location = addresses.FirstOrDefault();
 
-            var asd = await geocoder.ReverseGeocodeAsync(latitude, longitude);
+            if (location is null)
+            {
+                return string.Empty;
+            }
 
-            return "TODO";
+            return $"{location.AddressLine}, {location.PostalCode} {location.Locality}, {location.AdminDistrict}, {location.CountryRegion}";
         }
 
     }
