20fc8862292bcdcfde87caf9d2fcb92df279df96
Elias Martinez <elias.martinez@isolutions.ch>
2021-10-08 14:02:50 +0200

    addded domain and data

---
+++ src/backend/Tortilla.Hackaton.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Tortilla.Hackaton.Domain;
+
+namespace Tortilla.Hackaton.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasKey(u => u.Id);
+        }
+    }
+}
---
+++ src/backend/Tortilla.Hackaton.Domain/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Tortilla.Hackaton.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PasswordHash { get; set; }
+        public string DriversLicenseNumber { get; set; }
+        public int TotalCo2Saved { get; set; }
+        public int Points { get; set; }
+    }
+}
