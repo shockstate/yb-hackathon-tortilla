6a509d6a6ba5197961de9794b079991ccfbf886e
Guillem Bonafonte <guillem.bonafonte@isolutions.ch>
2021-10-08 17:29:13 +0200

    fix map

--- src/backend/Tortilla.Hackathon.Services/Models/Dtos/CreateUserDto.cs
+++ src/backend/Tortilla.Hackathon.Services/Models/Dtos/CreateUserDto.cs
@@ -9,7 +9,7 @@ namespace Tortilla.Hackathon.Services.Models.Dtos
         public string Password { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public DateTime DateOfBirth { get; set; } = DateTime.MinValue;
-        public string DriversLicenceNumber { get; set; } = string.Empty;
+        public string DriversLicenseNumber { get; set; } = string.Empty;
         public CarDto Car { get; set; } = new CarDto();
     }
 }
