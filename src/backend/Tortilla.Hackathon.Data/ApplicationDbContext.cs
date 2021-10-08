ace1fc1944424e98857401162a23f55819da7d7e
Elias Martinez <elias.martinez@isolutions.ch>
2021-10-08 14:19:00 +0200

    First migration added

--- src/backend/Tortilla.Hackathon.Data/ApplicationDbContext.cs
+++ src/backend/Tortilla.Hackathon.Data/ApplicationDbContext.cs
@@ -5,6 +5,8 @@ namespace Tortilla.Hackathon.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
