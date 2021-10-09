9108ed5897b3ea5ce7a16b54c86a0f71058a22b3
Elias Martinez <elias.martinez@isolutions.ch>
2021-10-09 04:37:46 +0200

    useless property deleted

--- src/backend/Tortilla.Hackathon.Services/Models/Dtos/PendingPassengerDto.cs
+++ src/backend/Tortilla.Hackathon.Services/Models/Dtos/PendingPassengerDto.cs
@@ -12,7 +12,6 @@ namespace Tortilla.Hackathon.Services.Models.Dtos
         public string OriginDescription { get; set; }
         public double OriginLatitude { get; set; }
         public double OriginLongitude { get; set; }
-        public int OriginDistanceInMeters { get; set; }
         public string DestinationDescription { get; set; }
         public double DestinationLatitude { get; set; }
         public double DestinationLongitude { get; set; }
