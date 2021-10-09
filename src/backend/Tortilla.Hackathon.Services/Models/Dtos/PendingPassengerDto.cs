ccc10e36319cd4502083eaf65f7dd8d5dd47b588
Elias Martinez <elias.martinez@isolutions.ch>
2021-10-09 04:36:49 +0200

    pending passengers

--- src/backend/Tortilla.Hackathon.Services/Models/Dtos/PendingPassengerDto.cs
+++ src/backend/Tortilla.Hackathon.Services/Models/Dtos/PendingPassengerDto.cs
@@ -1,19 +1,21 @@
 ﻿using System;
-using Tortilla.Hackathon.Domain;
 
 namespace Tortilla.Hackathon.Services.Models.Dtos
 {
     public class PendingPassengerDto
     {
-        public string TripId { get; set; }
+        public string PassengerId { get; set; }
+        public string DayTripId { get; set; }
         public string UserId { get; set; }
         public string UserFirstName { get; set; }
         public string UserLastName { get; set; }
-        public double TripOriginLatitude { get; set; }
-        public double TripOriginLongitude { get; set; }
-        public double TripDestinationLatitude { get; set; }
-        public double TripDestinationLongitude { get; set; }
-        public DateTime TripStartDateTime { get; set; }
-        public TripRecurrency TripRecurrency { get; set; }
+        public string OriginDescription { get; set; }
+        public double OriginLatitude { get; set; }
+        public double OriginLongitude { get; set; }
+        public int OriginDistanceInMeters { get; set; }
+        public string DestinationDescription { get; set; }
+        public double DestinationLatitude { get; set; }
+        public double DestinationLongitude { get; set; }
+        public DateTime DateTime { get; set; }
     }
 }
