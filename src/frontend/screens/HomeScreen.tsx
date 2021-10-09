9cf8a34508a056c4e8a7f2f0b7cba67f383d0693
Alvaro Pareja <alvaropareja@gmail.com>
2021-10-09 04:10:18 +0200

    Splash Screen

--- src/frontend/screens/HomeScreen.tsx
+++ src/frontend/screens/HomeScreen.tsx
@@ -1,13 +1,17 @@
 import React, { Component, ReactElement } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 import { RootStackScreenProps } from "../types";
+import logo from '../assets/images/logo.png';
 
 const HomeScreen = ({
   navigation,
 }: RootStackScreenProps<"Home">): ReactElement => {
   return (
     <View style={styles.container}>
+
+      <Image source={logo} style={{ width: 257, height: 59}}/>
+      
       <Text style={styles.title}>Welcome!</Text>
 
       <TouchableOpacity
